98861fa30d182b31c8990d6c1a1ac1546bc5c43b
BananEmad <33826865+BananEmad@users.noreply.github.com>
2017-12-16 19:26:32 +0200

    16

--- app/src/main/java/com/fci/delite/Logo.java
+++ app/src/main/java/com/fci/delite/Logo.java
@@ -1,6 +1,7 @@
 package com.fci.delite;
 
-
+import android.app.Activity;
+import android.os.Bundle;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.net.wifi.hotspot2.pps.HomeSp;
@@ -12,26 +13,22 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
-public class home extends AppCompatActivity {
-
-
+public class Logo extends AppCompatActivity {
     private static  int Splash_time_out=4000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
+        setContentView(R.layout.activity_logo);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run()
             {
-                Intent homeIntent=new Intent(home.this,Main.class);
+                Intent homeIntent=new Intent(Logo.this,Main.class);
                 startActivity(homeIntent);
                 finish();
             }
         },Splash_time_out);
-
-
     }
 }
--- app/src/main/java/com/fci/delite/SignU.java
+++ app/src/main/java/com/fci/delite/SignU.java
@@ -8,6 +8,6 @@ public class SignU extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_u);
+      // setContentView(R.layout.activity_sign_u);
     }
 }
