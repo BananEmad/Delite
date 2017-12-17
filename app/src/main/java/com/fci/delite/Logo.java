3b7d5b382741ec6e43a997b4de371bc534ed5d3f
BananEmad <33826865+BananEmad@users.noreply.github.com>
2017-12-17 22:22:14 +0200

    17

--- app/src/main/java/com/fci/delite/Logo.java
+++ app/src/main/java/com/fci/delite/Logo.java
@@ -1,17 +1,9 @@
 package com.fci.delite;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.app.ActionBar;
 import android.content.Intent;
-import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 public class Logo extends AppCompatActivity {
     private static  int Splash_time_out=4000;
@@ -25,7 +17,7 @@ public class Logo extends AppCompatActivity {
             @Override
             public void run()
             {
-                Intent homeIntent=new Intent(Logo.this,Main.class);
+                Intent homeIntent=new Intent(Logo.this,SignU.class);
                 startActivity(homeIntent);
                 finish();
             }
